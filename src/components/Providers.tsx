48d4b81f742791696beb2273926a5c6f29963c45
Susmita-Dey <susmitadey475@gmail.com>
2024-08-28 23:22:58 +0530

    fix typo in providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -7,21 +7,20 @@ import { httpBatchLink } from "@trpc/client";
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
-  const [trpcClient] = useState(
-    () =>
-      new trpc.createClient({
-        links: [
-          httpBatchLink({
-            url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
-            fetch(url, options) {
-              return fetch(url, {
-                ...options,
-                credentials: "include",
-              });
-            },
-          }),
-        ],
-      })
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
+          fetch(url, options) {
+            return fetch(url, {
+              ...options,
+              credentials: "include",
+            });
+          },
+        }),
+      ],
+    })
   );
 
   return (
