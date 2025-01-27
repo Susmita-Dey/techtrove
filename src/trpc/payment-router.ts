562a3836ff42d5985f53530a0b31f0ddb8010753
Susmita-Dey <susmitadey475@gmail.com>
2025-01-27 23:48:56 +0530

    trying to add stripe

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { privateProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { getPayloadClient } from "../get-payload";
+import { stripe } from "../lib/stripe";
+import type Stripe from "stripe";
 
 export const paymentRouter = router({
   createSession: privateProcedure
@@ -22,5 +24,39 @@ export const paymentRouter = router({
           id: { in: productIds },
         },
       });
+
+      const filteredProducts = products.filter((prod) => Boolean(prod.priceId));
+
+      const order = await payload.create({
+        collection: "orders",
+        data: {
+          _isPaid: false,
+          products: filteredProducts,
+          user: user.id,
+        },
+      });
+
+      const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+      line_items.push({
+        price: "price_1JZ3ZzJ9z1j1ZzJ9z1j1ZzJ",
+        quantity: 1,
+        adjustable_quantity: {
+          enabled: false,
+        },
+      });
+
+      try {
+        const stripeSession = await stripe.checkout.sessions.create({
+          success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+          cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
+          payment_method_types: ["card", "paypal"],
+          mode: "payment",
+          metadata: {
+            userId: user.id,
+            orderId: order.id,
+          },
+          line_items: line_items,
+        });
+      } catch (err) {}
     }),
 });
